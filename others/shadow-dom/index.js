02c0fb268970ea29db20eff914a16ec23dfa077f
pengVc <penjuntan@gmail.com>
2025-03-17 21:27:45 +0800

    feat(shadow-dom): 实现开放和封闭 Shadow DOM 的示例- 在 index.html 中添加了两个新的 div 元素，用于演示开放和封闭 Shadow DOM -重构了 index.js，创建了一个通用的 runShadow 函数来处理不同的 Shadow DOM 模式 - 在开放和封闭 Shadow DOM 中添加了事件监听器，展示了事件传播的差异 - 使用模板字符串和解构赋值提高了代码的可读性和简洁性

--- others/shadow-dom/index.js
+++ others/shadow-dom/index.js
@@ -1,36 +1,57 @@
-const app = document.querySelector('#app')
+const runShadow = (params) => {
+  const { hostId, mode = 'open' } = params ?? {}
 
-const shadow = app.attachShadow({ mode: 'open'})
+  const app = document.querySelector(`#${hostId}`)
 
-shadow.innerHTML = `
+  const shadow = app.attachShadow({ mode})
+
+  shadow.innerHTML = `
   <article>
-    <h2>hello shadow dom</h2>
+    <h2>hello ${mode} shadow dom</h2>
      <section>
       <button id="btn"> click me </button>
      </section>
   </article>
 `
+  console.log('shadow root', {
+    'shadow': shadow,
+    'shadow root': app.shadowRoot,
+  });
 
-const sdBtn = shadow.querySelector('#btn')
+  const sdBtn = shadow.querySelector('#btn')
 
-sdBtn.addEventListener('click', (event) => {
-  console.log('shadow dom btn clicked listener', {
-    'event.target': event.target,
-    'event.currentTarget': event.currentTarget,
+  sdBtn.addEventListener('click', (event) => {
+    console.log('shadow dom btn clicked listener', {
+      'event.target': event.target,
+      'event.currentTarget': event.currentTarget,
+    })
   })
+
+  app.addEventListener('click', (event) => {
+    console.log('shadow host clicked listener', {
+      'event.target': event.target,
+      'event.composedPath': event.composedPath(),
+      'event.currentTarget': event.currentTarget,
+    })
+  })
+
+
+  document.body.addEventListener('click', (event) => {
+    console.log('document.body clicked listener', {
+      'event.target': event.target,
+      'event.composedPath': event.composedPath(),
+      'event.currentTarget': event.currentTarget,
+    })
+  })
+
+}
+
+runShadow({
+  hostId: 'open-shadow-host',
+  mode: 'open'
 })
 
-app.addEventListener('click', (event) => {
-  console.log('shadow host clicked listener', {
-    'event.target': event.target,
-    'event.currentTarget': event.currentTarget,
-  })
-})
-
-
-document.body.addEventListener('click', (event) => {
-  console.log('document.body clicked listener', {
-    'event.target': event.target,
-    'event.currentTarget': event.currentTarget,
-  })
+runShadow({
+  hostId: 'closed-shadow-host',
+  mode: 'closed'
 })
